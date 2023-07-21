47c79ebb459cccf7f0544be5231c9814af930ce9
Marco De Vecchi <mdevecchi@alpenite.com>
2023-07-21 23:59:18 +0200

    fix

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -11,15 +11,15 @@ export default async function (req, res) {
     model: "text-davinci-003",
     prompt: reviewPrompt(req.body),
     max_tokens: 2000,
-    temperature: 0.9,
+    temperature: 0.2,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
 function reviewPrompt(ticket) {
   console.log(new Date().toLocaleDateString('it-IT'))
-  return `
-  estri le seguenti informazioni da ${ticket.product}
+  var ciao= `
+  estri le seguenti informazioni da: ${ticket.product}
   
   name: ${ticket.nome}
   surname: ${ticket.cognome}
@@ -35,14 +35,9 @@ function reviewPrompt(ticket) {
   la priorità può essere: alta, media, bassa
   lo stato puo' essere:aperto, chiuso, in lavorazione, in attesa, rifiutato
     la categoria è una stringa che può essere: hardware, software, rete, altro
-    oggi è il ${new Date().toLocaleDateString('it-IT')}  e il campo date in formato italiano
-    e  deve essere calcolato sulla base dell informazioni presenti su ${ticket} 
-    title è il topic del ticket massimo due parole
-    description è la descrizione del ticket
-  
-
-  
-  
-
+    oggi è il ${new Date().toLocaleDateString('it-IT')}  
   `
+  console.log(ciao);
+  return ciao;
+
 }
